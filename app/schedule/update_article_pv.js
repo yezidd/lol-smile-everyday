7575ca85602987839685eb3f7bf509b2ee158723
yzdd <3050232357@qq.com>
2019-05-31 14:23:43 +0800

    完成pv刷新

--- app/schedule/update_article_pv.js
+++ app/schedule/update_article_pv.js
@@ -1,7 +1,11 @@
 'use strict';
 
 const Subscription = require('egg').Subscription;
-
+const moment = require('moment');
+let pageNo = 2;
+let articleNum = 0;
+let pvAllNum = 0;
+let aimDate = moment().format('YYYY-MM-DD');
 // 每天抓取 文章列表，然后更新差量数据
 class UpdateArticlePv extends Subscription {
   // 通过 schedule 属性来设置定时任务的执行间隔等配置
@@ -10,12 +14,45 @@ class UpdateArticlePv extends Subscription {
       interval: '10s',
       type: 'all', // 指定所有的 worker 都需要执行,
       immediate: true,
-      disable: true,
+      disable: false,
     };
   }
   // subscribe 是真正定时任务执行时被运行的函数
   async subscribe() {
-    console.log('----');
+    // 如果是目标今天，那么就执行任务
+    // 如果不是，则不执行
+    if (aimDate === moment().format('YYYY-MM-DD')) {
+      try {
+        const data = await this.ctx.service.article.updateArticlePv(pageNo);
+        // 获取一页数据之后，更新总article数量和pv数量
+        articleNum = articleNum + data.list.length;
+        data.list.forEach(item => {
+          pvAllNum = pvAllNum + Number(item.pv);
+        });
+        if (Number(data.next) === 1) {
+          pageNo++;
+        } else {
+          // 最后一页的时候，存储汇总数据到数据库
+          // 今天定时任务完成
+          // 然后重置状态
+          await this.app.mysql.insert('all_record', {
+            article_num: articleNum,
+            article_pv_all: pvAllNum,
+            all_record_date: moment().format('YYYY-MM-DD'),
+          });
+          // 目标日期用明天
+          // 重置其他状态
+          aimDate = moment().add(1, 'days').format('YYYY-MM-DD');
+          pageNo = 1;
+          articleNum = 0;
+          pvAllNum = 0;
+        }
+      } catch (err) {
+        console.log(err, '====');
+      }
+    } else {
+      console.log('---今天的刷新pv数据任务完成');
+    }
   }
 }
 module.exports = UpdateArticlePv;
--- app/service/article.js
+++ app/service/article.js
@@ -1,11 +1,73 @@
 'use strict';
 const Service = require('egg').Service;
+const async = require('async');
+const moment = require('moment');
+
 class ArticleService extends Service {
+  // 获取article数据
   async getArticleData(pageNo) {
     const res = await this.ctx.curl(`http://qt.qq.com/lua/lol_news/mcn_author_news?author_uuid=59d4872a-461a-47a0-92de-de5c15973b7a&page=${pageNo}&num=5&type=0&plat=ios&version=9851`, {
       dataType: 'json',
     });
     return res.data;
+  }
+  // 更新article pv
+  async updateArticlePv(pageNo) {
+    const data = await this.getArticleData(pageNo);
+    const list = data.list;
+    // 使用aysnc库来异步执行
+    async.every(list, async item => {
+      try {
+        // 首先查询此条数据有没有存在article表中
+        // 如果不为null则比较pv量，如果没有则转存article和文章
+        const result = await this.app.mysql.get('article', { article_id: item.article_id });
+        const data = JSON.parse(JSON.stringify(result));
+        if (data) {
+          // 如果存在则更新
+          const updateResult = await this.app.mysql.update('article', { pv: item.pv, id: data.id });
+          console.log('---更新数据库', updateResult);
+          // 如果同一天已经有一条pv数据了,那么就不添加了
+          const searchData = await this.app.mysql.get('pv_record', { pv_record_date: moment().format('YYYY-MM-DD'), article_id: item.article_id });
+          if (searchData === null) {
+            // 然后查询pv_record表，看看是否已经有存在的数据了，
+            // 如果有则进行pv对比，如果pv没有更新，则直接更新日期
+            // 如果pv更新，则转存此条pv数据
+            if (Number(result.pv) === Number(item.pv)) {
+              await this.app.mysql.update('pv_record', { pv_record_date: moment().format('YYYY-MM-DD') }, { where: { article_id: item.article_id, pv_record_date: moment().subtract(1, 'days').format('YYYY-MM-DD') } });
+            } else {
+            // 添加数据到pv表中
+              await this.app.mysql.insert('pv_record', { article_id: item.article_id, pv: item.pv, pv_record_date: moment().format('YYYY-MM-DD') });
+            }
+          }
+        } else {
+          // 如果不存在添加数据到文章表中
+          const addResult = await this.app.mysql.insert('article', item);
+          console.log('---添加数据库', addResult);
+          // 如果同一天已经有一条pv数据了,那么就不添加了
+          const searchData = await this.app.mysql.get('pv_record', { pv_record_date: moment().format('YYYY-MM-DD'), article_id: item.article_id });
+          if (searchData === null) {
+            // 添加数据到pv表中
+            await this.app.mysql.insert('pv_record', { article_id: item.article_id, pv: item.pv, pv_record_date: moment().format('YYYY-MM-DD') });
+          }
+        }
+      } catch (err) {
+        console.log(err, '-');
+        // 如果发生错误，存储发生错误的原因和此时的时间
+        await this.app.mysql.insert('py_error', {
+          error_message: JSON.stringify(err),
+          article_id: item.article_id,
+          handle_type: 'ImageService/每日更新pv量',
+          current_time: moment().format('YYYY-MM-DD HH:mm:ss'),
+          is_result: 0,
+          result: '',
+        });
+      }
+    }, function(err) {
+      if (err === null) {
+        console.log(`一次接口数据pv刷新完成-->第${pageNo}页`);
+      }
+    });
+    return data;
   }
 }
 
